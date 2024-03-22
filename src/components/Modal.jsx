0656b04ed0810b4bf05d04dc94275706742d4820
Juan Ordonez <juan.ordonez@keypathedu.com>
2024-03-21 20:57:08 -0500

    updating data file with real videos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -19,7 +19,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: '55%',
+  width: '50%',
   height: '60%',
   bgcolor: 'background.paper',
   border: '2px solid #000',
